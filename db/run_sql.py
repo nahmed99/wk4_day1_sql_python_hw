f999c4562ba0ff347db38dc0dd4fb48cfa563271
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-23 18:44:12 +0000

    added db connections, and created cursors. Closed db connection and closed cursor too.

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,2 +1,22 @@
 import psycopg2
 import psycopg2.extras as ext
+
+def run_sql(sql, values = None):
+
+    conn = None
+    results = []
+
+    try:
+        conn=psycopg2.connect("dbname='task_manager'")
+        cur = conn.cursor(cursor_factory=ext.DictCursor)
+        cur.execute(sql, values)
+        cur.commit()
+        results = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return results
