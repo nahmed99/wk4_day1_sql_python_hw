776644ec51e8b3aa5c86bb5185e1619777b1fa1c
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-23 19:02:41 +0000

    added save function in task_repository

--- repositories/task_repository.py
+++ repositories/task_repository.py
@@ -1,10 +1,20 @@
 from db.run_sql import run_sql
+from models.task import Task 
 
 def save(task):
-
-    # note duration and completed don't need quotes
-    # sql = f"INSERT INTO tasks (description, assignee, duration, completed) VALUES ( '{task.description}', '{task.assignee}', {task.duration}, {task.completed} )" 
-
     sql = "INSERT INTO tasks (description, assignee, duration, completed) VALUES (%s, %s, %s, %s)"
     values = [task.description, task.assignee, task.duration, task.completed]
     run_sql(sql, values)
+
+
+def select_all():  
+    tasks = []  # ADDED - in case we get `None` back from run_sql
+
+    sql = "SELECT * FROM tasks"
+    results = run_sql(sql)
+
+    for row in results:
+        task = Task(row['description'], row['assignee'], row['duration'], row['completed'], row['id'] )
+        tasks.append(task)
+    return tasks
+
